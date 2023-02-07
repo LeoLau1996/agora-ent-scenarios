e2b334e4f59ebd90dddfd82272269aa870c0e33e
xucz <xgfd3@users.noreply.github.com>
2023-02-07 16:48:40 +0800

    [Show][Android]perfect debug setting ui.

--- Android/scenes/show/src/main/java/io/agora/scene/show/widget/DebugSettingDialog.kt
+++ Android/scenes/show/src/main/java/io/agora/scene/show/widget/DebugSettingDialog.kt
@@ -2,8 +2,7 @@ package io.agora.scene.show.widget
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.inputmethod.EditorInfo
-import io.agora.scene.base.utils.ToastUtils
+import android.widget.EditText
 import io.agora.scene.show.RtcEngineInstance
 import io.agora.scene.show.databinding.ShowWidgetDebugSettingDialogBinding
 
@@ -24,80 +23,37 @@ class DebugSettingDialog(context: Context) : BottomFullDialog(context) {
             dismiss()
         }
         // 采集帧率
-        mBinding.etFpsCapture.setText(RtcEngineInstance.videoCaptureConfiguration.captureFormat.fps.toString())
-        mBinding.etFpsCapture.setOnEditorActionListener { v, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                RtcEngineInstance.videoCaptureConfiguration.captureFormat.fps = mBinding.etFpsCapture.text.toString().toIntOrNull()?: 30
-                RtcEngineInstance.rtcEngine.setCameraCapturerConfiguration(RtcEngineInstance.videoCaptureConfiguration)
-                v.clearFocus()
-                ToastUtils.showToast("设置成功")
-            }
-            return@setOnEditorActionListener false
-        }
-
+        setText(mBinding.etFpsCapture, RtcEngineInstance.videoCaptureConfiguration.captureFormat.fps.toString())
         // 采集分辨率
-        mBinding.etResolutionWidthCapture.setText(RtcEngineInstance.videoCaptureConfiguration.captureFormat.width.toString())
-        mBinding.etResolutionHeightCapture.setText(RtcEngineInstance.videoCaptureConfiguration.captureFormat.height.toString())
-        mBinding.etResolutionWidthCapture.setOnEditorActionListener { v, actionId, event ->
-            if(actionId == EditorInfo.IME_ACTION_NEXT){
-                mBinding.etResolutionHeightCapture.requestFocus()
-            }
-            return@setOnEditorActionListener false
-        }
-        mBinding.etResolutionHeightCapture.setOnEditorActionListener { v, actionId, event ->
-            if(actionId == EditorInfo.IME_ACTION_DONE){
-                RtcEngineInstance.videoCaptureConfiguration.captureFormat.width = mBinding.etResolutionWidthCapture.text.toString().toIntOrNull()?: 720
-                RtcEngineInstance.videoCaptureConfiguration.captureFormat.height = mBinding.etResolutionHeightCapture.text.toString().toIntOrNull()?: 1080
-                RtcEngineInstance.rtcEngine.setCameraCapturerConfiguration(RtcEngineInstance.videoCaptureConfiguration)
-                ToastUtils.showToast("设置成功")
-                v.clearFocus()
-            }
-            return@setOnEditorActionListener false
-        }
+        setText(mBinding.etResolutionWidthCapture, RtcEngineInstance.videoCaptureConfiguration.captureFormat.width.toString())
+        setText(mBinding.etResolutionHeightCapture, RtcEngineInstance.videoCaptureConfiguration.captureFormat.height.toString())
 
         // 帧率
-        mBinding.etFps.setText(RtcEngineInstance.videoEncoderConfiguration.frameRate.toString())
-        mBinding.etFps.setOnEditorActionListener { v, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                RtcEngineInstance.videoEncoderConfiguration.frameRate = mBinding.etFps.text.toString().toIntOrNull()?: 30
-                RtcEngineInstance.rtcEngine.setVideoEncoderConfiguration(RtcEngineInstance.videoEncoderConfiguration)
-                v.clearFocus()
-                ToastUtils.showToast("设置成功")
-            }
-            return@setOnEditorActionListener false
-        }
-        
+        setText(mBinding.etFps, RtcEngineInstance.videoEncoderConfiguration.frameRate.toString())
         // 分辨率
-        mBinding.etResolutionWidth.setText(RtcEngineInstance.videoEncoderConfiguration.dimensions.width.toString())
-        mBinding.etResolutionHeight.setText(RtcEngineInstance.videoEncoderConfiguration.dimensions.height.toString())
-        mBinding.etResolutionWidth.setOnEditorActionListener { v, actionId, event ->
-            if(actionId == EditorInfo.IME_ACTION_NEXT){
-                mBinding.etResolutionHeight.requestFocus()
-            }
-            return@setOnEditorActionListener false
-        }
-        mBinding.etResolutionHeight.setOnEditorActionListener { v, actionId, event ->
-            if(actionId == EditorInfo.IME_ACTION_DONE){
-                RtcEngineInstance.videoEncoderConfiguration.dimensions.width = mBinding.etResolutionWidth.text.toString().toIntOrNull()?: 720
-                RtcEngineInstance.videoEncoderConfiguration.dimensions.height = mBinding.etResolutionHeight.text.toString().toIntOrNull()?: 1080
-                RtcEngineInstance.rtcEngine.setVideoEncoderConfiguration(RtcEngineInstance.videoEncoderConfiguration)
-                ToastUtils.showToast("设置成功")
-                v.clearFocus()
-            }
-            return@setOnEditorActionListener false
-        }
-
+        setText(mBinding.etResolutionWidth, RtcEngineInstance.videoEncoderConfiguration.dimensions.width.toString())
+        setText(mBinding.etResolutionHeight, RtcEngineInstance.videoEncoderConfiguration.dimensions.height.toString())
         // 码率
-        mBinding.etBitrate.setText(RtcEngineInstance.videoEncoderConfiguration.bitrate.toString())
-        mBinding.etBitrate.setOnEditorActionListener { v, actionId, event ->
-            if(actionId == EditorInfo.IME_ACTION_DONE){
-                RtcEngineInstance.videoEncoderConfiguration.bitrate = mBinding.etBitrate.text.toString().toIntOrNull()?: 720
-                RtcEngineInstance.rtcEngine.setVideoEncoderConfiguration(RtcEngineInstance.videoEncoderConfiguration)
-                ToastUtils.showToast("设置成功")
-                v.clearFocus()
-            }
-            return@setOnEditorActionListener false
-        }
+        setText(mBinding.etBitrate, RtcEngineInstance.videoEncoderConfiguration.bitrate.toString())
 
+        mBinding.tvSure.setOnClickListener {
+            RtcEngineInstance.videoCaptureConfiguration.captureFormat.fps = mBinding.etFpsCapture.text.toString().toIntOrNull()?: 30
+            RtcEngineInstance.videoCaptureConfiguration.captureFormat.width = mBinding.etResolutionWidthCapture.text.toString().toIntOrNull()?: 720
+            RtcEngineInstance.videoCaptureConfiguration.captureFormat.height = mBinding.etResolutionHeightCapture.text.toString().toIntOrNull()?: 1080
+            RtcEngineInstance.rtcEngine.setCameraCapturerConfiguration(RtcEngineInstance.videoCaptureConfiguration)
+
+            RtcEngineInstance.videoEncoderConfiguration.frameRate = mBinding.etFps.text.toString().toIntOrNull()?: 30
+            RtcEngineInstance.videoEncoderConfiguration.dimensions.width = mBinding.etResolutionWidth.text.toString().toIntOrNull()?: 720
+            RtcEngineInstance.videoEncoderConfiguration.dimensions.height = mBinding.etResolutionHeight.text.toString().toIntOrNull()?: 1080
+            RtcEngineInstance.videoEncoderConfiguration.bitrate = mBinding.etBitrate.text.toString().toIntOrNull()?: 720
+            RtcEngineInstance.rtcEngine.setVideoEncoderConfiguration(RtcEngineInstance.videoEncoderConfiguration)
+
+            dismiss()
+        }
+    }
+
+    private fun setText(editText: EditText, content: String){
+        editText.setText(content)
+        editText.setSelection(content.length)
     }
 }
